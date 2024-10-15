092789e9591be233cad5f87f72db62bb112c9028
sanghoon <tkdgns5817@gmail.com>
2024-10-16 08:09:39 +0900

    chore: test

--- src/main/java/com/qticket/gateway/config/CorsConfig.java
+++ src/main/java/com/qticket/gateway/config/CorsConfig.java
@@ -11,13 +11,14 @@ public class CorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:19092");
-        config.addAllowedOrigin("http://localhost:19093");
-        config.addAllowedOrigin("http://localhost:19094");
-        config.addAllowedOrigin("http://localhost:19095");
+        config.addAllowedOriginPattern("*");
+//        config.addAllowedOrigin("http://localhost:19092");
+//        config.addAllowedOrigin("http://localhost:19093");
+//        config.addAllowedOrigin("http://localhost:19094");
+//        config.addAllowedOrigin("http://localhost:19095");
         config.addAllowedMethod("*"); // 허용할 HTTP 메서드, 예시로 모든 메서드 허용
         config.addAllowedHeader("*"); // 허용할 Header, 예시로 모든 Header 허용
-        config.setAllowCredentials(true); // 쿠키와 같은 자격 증명 허용 여부
+        config.setAllowCredentials(false); // 쿠키와 같은 자격 증명 허용 여부
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config); // 모든 경로에 대해 CORS 설정 적용
